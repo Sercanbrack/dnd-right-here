bd9854a76e313269eb6133fa1abe856fb91c7e54
Jeremy Johnson <jeremyjohnson@Jeremys-Mac-mini.local>
2022-07-25 17:19:28 -0600

    added info for the 3 models. I also made some temp info for user seeds and character seed

---
+++ Main/seeds/seed.js
@@ -0,0 +1,9 @@
+const sequelize = require('../config/connection');
+const { User, Character } = require('../models');
+
+const userData = require('./userData.json');
+const characterData = require('./characterData.json');
+
+const seedDatabase = // I'm not sure how to continue here.
+  
+  seedDatabase();
